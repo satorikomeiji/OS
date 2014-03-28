34662e9b96c39832f1364846bccd5b09a7ea1013
Ilya Kuznetsov <diablomobile.modest@gmail.com>
2014-03-28 16:14:35 +0400

    fd

--- fdscat.c
+++ fdscat.c
@@ -5,13 +5,13 @@
 const int BUFFER_SIZE = 4096;
 const int TRY_COUNT = 10;
 
-int main(int argc, char ** argv){
+int main(int argc, char ** argv) {
     char buf [BUFFER_SIZE];
     for(int i = 0; i < argc - 1; ++i) {
         int fd;
         sscanf(argv[i + 1], "%d", &fd);
         int ret;
-        while((ret = read(fd, buf, BUFFER_SIZE)) != 0){
+        while((ret = read(fd, buf, BUFFER_SIZE)) != 0) {
             if (ret == -1) {
                 char str[] = "Error read";
                 perror(str);
@@ -20,18 +20,18 @@ int main(int argc, char ** argv){
                 int ret2;
                 int remained = ret;
                 int tries = 0;
-                while(remained > 0){
-                    ret2 = write(STDOUT_FILENO, buf + ret - remained, remained);
+                while(remained > 0) {
+                    ret2 = write(1, buf + ret - remained, remained);
                     if(ret2 < 0) {
                         char str[] = "Error write";
                         perror(str);
                         return EXIT_FAILURE;
                     } else if(ret2 == 0) {
                         ++tries;
-                        if(tries == TRY_COUNT){
+                        if(tries == TRY_COUNT) {
                             return EXIT_FAILURE;
                         }
-                    }else{
+                    } else {
                         tries = 0;
                     }
                     remained -= ret2;
